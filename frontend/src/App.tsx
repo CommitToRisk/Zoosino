e96926eb2ca899251d784632aa6d8d30d66f00d8
Martin <martinpop06@gmail.com>
2026-02-05 17:46:47 +0100

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-5xl font-bold text-pink-500 underline decoration-sky-500">
+      TEST
+      </p>
+    </>
+  )
+}
+
+export default App
